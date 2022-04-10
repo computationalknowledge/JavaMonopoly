6d625888d3b4880e35aa7c675f84c92008601570
The Prof <52168158+computationalknowledge@users.noreply.github.com>
2022-04-10 13:01:58 -0400

    Create starterCode.java

---
+++ starterCode.java
@@ -0,0 +1,31 @@
+
+public class TheMonopolyGame {
+
+	public static void main(String[] args) {
+		TheBoard game = new TheBoard();
+
+	}
+
+}
+
+class TheBoard{
+	
+	
+	
+	public TheBoard() {
+		
+	}
+	
+}
+
+class BoardSquare{
+	
+}
+
+class Dice{
+	
+	public int ReturnThrowValue() {
+		return 0;
+	}
+	
+}
